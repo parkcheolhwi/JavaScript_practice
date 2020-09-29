d9a5243a4a9bb2ddb3b3a8aba2166788f596a075
parkcheolhwi <cjfgnl9205@gmail.com>
2020-09-29 12:51:04 +0900

    update

--- socket.io/app.js
+++ socket.io/app.js
@@ -7,8 +7,8 @@ app.get('/', (req, res) => {
 });
 
 io.on ( 'connection' , (socket) => { 
-  socket.on ( 'chat message' , (msg) => { 
-    io.emit ( 'chat message' , msg); 
+  socket.on ( 'chat message' , (data) => { 
+    io.emit ( 'chat message' , data); 
   }); 
 });
 
